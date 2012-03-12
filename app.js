753c8ea59f67639a312a75bc29d6b6338c74f1cf
Aakriti Gupta <aakriti@aakritigupta.com>
2012-03-12 22:51:47 +0530

    added (commented ou)t code for requiring votekit

--- app.js
+++ app.js
@@ -5,9 +5,16 @@
 
 var express = require('express')
   , routes = require('./routes');
+//  , votekit = require('./votekit');
 
-var app = module.exports = express.createServer();
+//create server
+var app = module.exports = express.createServer('127.0.0.1');
+//connect to the votekit engine
+//votekit = require('votekit-engine');
+//votekit.connect('localhost', 'newdb');
 
+//connect = require('connect');
+//auth = require('connect-auth');//what iis this for?
 // Configuration
 
 app.configure(function(){
