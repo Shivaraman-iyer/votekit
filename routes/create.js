4a940f793d3db38430f17bd2d86ad3e4a31a719f
Aakriti Gupta <aakriti@aakritigupta.com>
2012-03-31 02:18:49 +0530

    Removed useless comments. Correction in previous commit msg: No re-direction needed while handling a GET request.

--- routes/create.js
+++ routes/create.js
@@ -1,12 +1,5 @@
 app.post('/api/poll', function(req, res){
-    //Create an object which reads data from req
-//    var poll = {
-  //      poll_issue: req.body.poll_issue//How is poll issue passed in req?
-   // };
-  //  console.log('Data rcvd: ', req.body);    
-    //Publish this poll
-
-        publish(req.body, function(err, pollId){
+    publish(req.body, function(err, pollId){
         if(err){
             console.log(err.message);
             res.redirect('/');
