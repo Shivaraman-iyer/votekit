779e5d83b081446a31262b0730a2f78c45905ef3
Aakriti Gupta <aakriti@aakritigupta.com>
2012-04-13 23:11:52 +0530

    CHanged url for updating likes/dislikes

--- routes/update.js
+++ routes/update.js
@@ -6,37 +6,300 @@
  * id: <>//Id of chooser
  * }
  */
-app.post('/api/poll/update/:id', function(req, res){
- //  console.log('You want to update: \n', req.params.id);
-   //console.log('New data: ', req.body);
-   if(req.body.choice === 'like')
-  updatePostById_likes(req.params.id, req.body, function(err, numAffected){
-   
-    if(err){
-          console.log(err.message);
-          callback(err);    
-        }
-        else{
-            console.log(numAffected, ' docs updated.\n');
-	    
-        }
-        });
-
-else
-  updatePostById_dislikes(req.params.id, req.body, function(err, numAffected){
-   
-    if(err){
-      
-            console.log(err.message);
-	    callback(err);
-    }
-        else{
-            console.log(numAffected, ' docs updated.\n');
-
-        }
-        });
+app.post('/api/poll/update/like/1/:id/', function(req, res){
+   VSchemas.Post.findById(req.params.id, function( err, post){
+    VSchemas.Poll.update({_id:post.poll}, {$push: {'options_list.0.who_likes':req.body.id}}, {upsert:true}, function(err, numAffected){
+  if(err){
+    console.log(err.message);
+    //callback(err);
+  }
+  else{
+    console.log(numAffected);
+    //callback(null, numAffected);
+  }
+});
+});
+});
+app.post('/api/poll/update/like/2/:id', function(req, res){
+  console.log('posting');
+   VSchemas.Post.findById(req.params.id, function( err, post){
+    VSchemas.Poll.update({_id:post.poll}, {$push: {'options_list.1.who_likes':req.body.id}}, {upsert:true}, function(err, numAffected){
+  if(err){
+    console.log(err.message);
+    //callback(err);
+  }
+  else{
+    console.log(numAffected);
+    //callback(null, numAffected);
+  }
+});
+});
 });
 
+
+app.post('/api/poll/update/like/3/:id/', function(req, res){
+   VSchemas.Post.findById(req.params.id, function( err, post){
+    VSchemas.Poll.update({_id:post.poll}, {$push: {'options_list.2.who_likes':req.body.id}}, {upsert:true}, function(err, numAffected){
+  if(err){
+    console.log(err.message);
+    //callback(err);
+  }
+  else{
+    console.log(numAffected);
+    //callback(null, numAffected);
+  }
+});
+});
+});
+app.post('/api/poll/update/like/4/:id', function(req, res){
+  console.log('posting');
+   VSchemas.Post.findById(req.params.id, function( err, post){
+    VSchemas.Poll.update({_id:post.poll}, {$push: {'options_list.3.who_likes':req.body.id}}, {upsert:true}, function(err, numAffected){
+  if(err){
+    console.log(err.message);
+    //callback(err);
+  }
+  else{
+    console.log(numAffected);
+    //callback(null, numAffected);
+  }
+});
+});
+});
+app.post('/api/poll/update/like/5/:id/', function(req, res){
+   VSchemas.Post.findById(req.params.id, function( err, post){
+    VSchemas.Poll.update({_id:post.poll}, {$push: {'options_list.4.who_likes':req.body.id}}, {upsert:true}, function(err, numAffected){
+  if(err){
+    console.log(err.message);
+    //callback(err);
+  }
+  else{
+    console.log(numAffected);
+    //callback(null, numAffected);
+  }
+});
+});
+});
+app.post('/api/poll/update/like/6/:id', function(req, res){
+  console.log('posting');
+   VSchemas.Post.findById(req.params.id, function( err, post){
+    VSchemas.Poll.update({_id:post.poll}, {$push: {'options_list.5.who_likes':req.body.id}}, {upsert:true}, function(err, numAffected){
+  if(err){
+    console.log(err.message);
+    //callback(err);
+  }
+  else{
+    console.log(numAffected);
+    //callback(null, numAffected);
+  }
+});
+});
+});
+app.post('/api/poll/update/like/7/:id/', function(req, res){
+   VSchemas.Post.findById(req.params.id, function( err, post){
+    VSchemas.Poll.update({_id:post.poll}, {$push: {'options_list.6.who_likes':req.body.id}}, {upsert:true}, function(err, numAffected){
+  if(err){
+    console.log(err.message);
+    //callback(err);
+  }
+  else{
+    console.log(numAffected);
+    //callback(null, numAffected);
+  }
+});
+});
+});
+app.post('/api/poll/update/like/8/:id', function(req, res){
+  console.log('posting');
+   VSchemas.Post.findById(req.params.id, function( err, post){
+    VSchemas.Poll.update({_id:post.poll}, {$push: {'options_list.7.who_likes':req.body.id}}, {upsert:true}, function(err, numAffected){
+  if(err){
+    console.log(err.message);
+    //callback(err);
+  }
+  else{
+    console.log(numAffected);
+    //callback(null, numAffected);
+  }
+});
+});
+});
+app.post('/api/poll/update/like/9/:id/', function(req, res){
+   VSchemas.Post.findById(req.params.id, function( err, post){
+    VSchemas.Poll.update({_id:post.poll}, {$push: {'options_list.8.who_likes':req.body.id}}, {upsert:true}, function(err, numAffected){
+  if(err){
+    console.log(err.message);
+    //callback(err);
+  }
+  else{
+    console.log(numAffected);
+    //callback(null, numAffected);
+  }
+});
+});
+});
+app.post('/api/poll/update/like/10/:id', function(req, res){
+  console.log('posting');
+   VSchemas.Post.findById(req.params.id, function( err, post){
+    VSchemas.Poll.update({_id:post.poll}, {$push: {'options_list.9.who_likes':req.body.id}}, {upsert:true}, function(err, numAffected){
+  if(err){
+    console.log(err.message);
+    //callback(err);
+  }
+  else{
+    console.log(numAffected);
+    //callback(null, numAffected);
+  }
+});
+});
+});
+
+app.post('/api/poll/update/dislike/1/:id/', function(req, res){
+   VSchemas.Post.findById(req.params.id, function( err, post){
+    VSchemas.Poll.update({_id:post.poll}, {$push: {'options_list.0.who_dislikes':req.body.id}}, {upsert:true}, function(err, numAffected){
+  if(err){
+    console.log(err.message);
+    //callback(err);
+  }
+  else{
+    console.log(numAffected);
+    //callback(null, numAffected);
+  }
+});
+});
+});
+app.post('/api/poll/update/dislike/2/:id', function(req, res){
+  console.log('posting');
+   VSchemas.Post.findById(req.params.id, function( err, post){
+    VSchemas.Poll.update({_id:post.poll}, {$push: {'options_list.1.who_dislikes':req.body.id}}, {upsert:true}, function(err, numAffected){
+  if(err){
+    console.log(err.message);
+    //callback(err);
+  }
+  else{
+    console.log(numAffected);
+    //callback(null, numAffected);
+  }
+});
+});
+});
+
+app.post('/api/poll/update/dislike/3/:id/', function(req, res){
+   VSchemas.Post.findById(req.params.id, function( err, post){
+    VSchemas.Poll.update({_id:post.poll}, {$push: {'options_list.2.who_dislikes':req.body.id}}, {upsert:true}, function(err, numAffected){
+  if(err){
+    console.log(err.message);
+    //callback(err);
+  }
+  else{
+    console.log(numAffected);
+    //callback(null, numAffected);
+  }
+});
+});
+});
+app.post('/api/poll/update/dislike/4/:id', function(req, res){
+  console.log('posting');
+   VSchemas.Post.findById(req.params.id, function( err, post){
+    VSchemas.Poll.update({_id:post.poll}, {$push: {'options_list.3.who_dislikes':req.body.id}}, {upsert:true}, function(err, numAffected){
+  if(err){
+    console.log(err.message);
+    //callback(err);
+  }
+  else{
+    console.log(numAffected);
+    //callback(null, numAffected);
+  }
+});
+});
+});
+app.post('/api/poll/update/dislike/5/:id/', function(req, res){
+   VSchemas.Post.findById(req.params.id, function( err, post){
+    VSchemas.Poll.update({_id:post.poll}, {$push: {'options_list.4.who_dislikes':req.body.id}}, {upsert:true}, function(err, numAffected){
+  if(err){
+    console.log(err.message);
+    //callback(err);
+  }
+  else{
+    console.log(numAffected);
+    //callback(null, numAffected);
+  }
+});
+});
+});
+app.post('/api/poll/update/dislike/6/:id', function(req, res){
+  console.log('posting');
+   VSchemas.Post.findById(req.params.id, function( err, post){
+    VSchemas.Poll.update({_id:post.poll}, {$push: {'options_list.5.who_dislikes':req.body.id}}, {upsert:true}, function(err, numAffected){
+  if(err){
+    console.log(err.message);
+    //callback(err);
+  }
+  else{
+    console.log(numAffected);
+    //callback(null, numAffected);
+  }
+});
+});
+});
+app.post('/api/poll/update/dislike/7/:id/', function(req, res){
+   VSchemas.Post.findById(req.params.id, function( err, post){
+    VSchemas.Poll.update({_id:post.poll}, {$push: {'options_list.6.who_dislikes':req.body.id}}, {upsert:true}, function(err, numAffected){
+  if(err){
+    console.log(err.message);
+    //callback(err);
+  }
+  else{
+    console.log(numAffected);
+    //callback(null, numAffected);
+  }
+});
+});
+});
+app.post('/api/poll/update/dislike/8/:id', function(req, res){
+  console.log('posting');
+   VSchemas.Post.findById(req.params.id, function( err, post){
+    VSchemas.Poll.update({_id:post.poll}, {$push: {'options_list.7.who_dislikes':req.body.id}}, {upsert:true}, function(err, numAffected){
+  if(err){
+    console.log(err.message);
+    //callback(err);
+  }
+  else{
+    console.log(numAffected);
+    //callback(null, numAffected);
+  }
+});
+});
+});
+app.post('/api/poll/update/dislike/9/:id/', function(req, res){
+   VSchemas.Post.findById(req.params.id, function( err, post){
+    VSchemas.Poll.update({_id:post.poll}, {$push: {'options_list.8.who_dislikes':req.body.id}}, {upsert:true}, function(err, numAffected){
+  if(err){
+    console.log(err.message);
+    //callback(err);
+  }
+  else{
+    console.log(numAffected);
+    //callback(null, numAffected);
+  }
+});
+});
+});
+app.post('/api/poll/update/dislike/10/:id', function(req, res){
+  console.log('posting');
+   VSchemas.Post.findById(req.params.id, function( err, post){
+    VSchemas.Poll.update({_id:post.poll}, {$push: {'options_list.9.who_dislikes':req.body.id}}, {upsert:true}, function(err, numAffected){
+  if(err){
+    console.log(err.message);
+    //callback(err);
+  }
+  else{
+    console.log(numAffected);
+    //callback(null, numAffected);
+  }
+});
+});
+});
 app.post('/api/poll/expire/:id', function(req, res){
   if(req.body.expiry_date != undefined)
     updateExpiryDate(req.params.id, req.body, function(err, numAffected){
